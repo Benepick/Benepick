4d4155072298993fb247e0df838ec1217918c248
psg980331@naver.com <psg980331@naver.com>
2023-08-31 17:12:58 +0900

    Init: 백엔드 프로젝트 생성

---
+++ backend/benepick/src/main/java/com/ssafy/benepick/BenepickApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.benepick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenepickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenepickApplication.class, args);
+	}
+
+}
---
+++ backend/benepick/src/test/java/com/ssafy/benepick/BenepickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.benepick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenepickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
