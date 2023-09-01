1c6e9c5c2a2d35a85a426820cf9442b7db7c3521
psg980331@naver.com <psg980331@naver.com>
2023-09-01 15:00:54 +0900

    Feat: CardCompany Entity 생성

---
+++ backend/src/main/java/com/ssafy/benepick/domain/card/entity/CardCompany.java
@@ -0,0 +1,39 @@
+package com.ssafy.benepick.domain.card.entity;
+
+import java.util.List;
+
+import com.ssafy.benepick.domain.mydata.entity.MyDataCard;
+import com.ssafy.benepick.domain.mydata.entity.MyDataPayment;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class CardCompany {
+
+	@Id
+	private Long cardCompanyId;
+
+	@Column(nullable = false)
+	private String cardCompanyImgUrl;
+
+	@Column(nullable = false)
+	private String cardCompanyName;
+
+	@OneToMany(mappedBy = "CardCompany", cascade = CascadeType.ALL , orphanRemoval = true)
+	private List<MyDataCard> myDataCardList;
+
+	@OneToMany(mappedBy = "CardCompany", cascade = CascadeType.ALL , orphanRemoval = true)
+	private List<Card> cardList;
+}
