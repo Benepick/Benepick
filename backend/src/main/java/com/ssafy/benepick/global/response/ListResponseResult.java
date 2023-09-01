f665cc8ccd30f67fff552542cd1e3ea351446821
psg980331@naver.com <psg980331@naver.com>
2023-09-01 13:11:25 +0900

    Feat: 공통 ResponseResult 정의

---
+++ backend/src/main/java/com/ssafy/benepick/global/response/ListResponseResult.java
@@ -0,0 +1,17 @@
+package com.ssafy.benepick.global.response;
+
+import java.util.List;
+
+import lombok.Getter;
+
+@Getter
+public class ListResponseResult<T> extends ResponseResult {
+
+	private List<T> data;
+
+	public ListResponseResult(List<T> data) {
+		super(successResponse.statusCode, successResponse.messages, successResponse.developerMessage,
+			successResponse.timestamp);
+		this.data = data;
+	}
+}
