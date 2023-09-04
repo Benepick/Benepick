c4e0161b28da256933ff39b682f6d78b35343a5f
psg980331@naver.com <psg980331@naver.com>
2023-09-04 14:55:33 +0900

    Feat: 간편 비밀번호 변경 Dto 생성

--- backend/src/main/java/com/ssafy/benepick/domain/user/entity/User.java
+++ backend/src/main/java/com/ssafy/benepick/domain/user/entity/User.java
@@ -47,4 +47,8 @@ public class User {
 
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL , orphanRemoval = true)
 	private List<UserCardCompany> userCardCompanyList;
+
+	public void changeSimplePassword(String userSimplePassword){
+		this.userSimplePassword = userSimplePassword;
+	}
 }
