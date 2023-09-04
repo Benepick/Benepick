16c0483ffbb45772dbd565cff7412b68dddeec22
yongseong2 <ancjs369@naver.com>
2023-09-04 17:42:44 +0900

    Feat: 아이콘 버튼 구현

---
+++ frontend/src/common/assets/icons/iconIndex.ts
@@ -0,0 +1,10 @@
+export { default as Payment } from './Payment.svg';
+export { default as Card } from './Card.svg';
+export { default as Diamond } from './Diamond.svg';
+export { default as Home } from './Home.svg';
+export { default as Menu } from './Menu.svg';
+export { default as ChatBot } from './ChatBot.svg';
+export { default as Notification } from './Notification.svg';
+export { default as User } from './User.svg';
+export { default as Lock } from './Lock.svg';
+export { default as FingerPrint } from './FingerPrint.svg';
--- frontend/src/pages/Test/Test.tsx
+++ frontend/src/pages/Test/Test.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 import AlarmButton from '../../common/components/AlarmButton';
 
+import IconButton from '@common/components/IconButton';
+
 function Test() {
   return (
     <View>
       <AlarmButton isAlarmed={false} onPress={() => console.log('알람크릭')} />
+      <IconButton name="ChatBot" size={100} />
     </View>
   );
 }
