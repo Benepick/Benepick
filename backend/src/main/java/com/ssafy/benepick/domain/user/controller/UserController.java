310e521e6e79ec22bdb63d58a4d9d95f276c4ee9
psg980331@naver.com <psg980331@naver.com>
2023-09-04 14:28:28 +0900

    Feat: 로그인 기능 구현

--- backend/src/main/java/com/ssafy/benepick/domain/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/benepick/domain/user/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.benepick.domain.user.dto.request.CreateUserAccountRequestDto;
+import com.ssafy.benepick.domain.user.dto.request.LoginRequestDto;
 import com.ssafy.benepick.domain.user.service.UserService;
 import com.ssafy.benepick.global.response.ResponseResult;
 
@@ -15,6 +16,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -44,5 +46,21 @@ public class UserController {
 		log.info("UserController_createUserAccount | 사용자의 회원가입");
 		userService.createUserAccount(createUserAccountRequestDto , response);
 		return ResponseResult.successResponse;
+	}
+
+	@Operation(summary = "로그인", description = "사용자가 간편 비밀번호를 가지고 로그인 합니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "로그인 성공"),
+		@ApiResponse(responseCode = "400", description = "로그인 실패"),
+		@ApiResponse(responseCode = "460", description = "CI와 일치하는 유저가 존재하지 않음"),
+		@ApiResponse(responseCode = "461", description = "Access Token 이 존재하지 않습니다.")
+	})
+	@PostMapping("/login")
+	public ResponseResult login(
+		@Valid @RequestBody LoginRequestDto loginRequestDto , HttpServletRequest request) {
+		log.info("UserController_login | 사용자의 로그인");
+		if(userService.login(loginRequestDto , request))
+			return ResponseResult.successResponse;
+		return ResponseResult.failResponse;
 	}
 }
