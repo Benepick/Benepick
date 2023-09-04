44514b7353eca14e8533ac775d438d7127970e5b
psg980331@naver.com <psg980331@naver.com>
2023-09-04 11:38:31 +0900

    Feat: 회원가입 API 기능 구현

--- backend/src/main/java/com/ssafy/benepick/domain/user/service/UserService.java
+++ backend/src/main/java/com/ssafy/benepick/domain/user/service/UserService.java
@@ -1,4 +1,10 @@
 package com.ssafy.benepick.domain.user.service;
 
+import com.ssafy.benepick.domain.user.dto.request.CreateUserAccountRequestDto;
+
+import jakarta.servlet.http.HttpServletResponse;
+
 public interface UserService {
+	void createUserAccount(CreateUserAccountRequestDto createUserAccountRequestDto , HttpServletResponse response);
+	void setToken(String userId ,HttpServletResponse response);
 }
