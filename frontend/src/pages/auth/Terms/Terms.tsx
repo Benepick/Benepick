0382e7b6cc10e1d81a86900fde7fb53162065d5d
ikthegreat <ikthegreat@icloud.com>
2023-09-05 13:21:55 +0900

    Feat: 이용약관 스크롤 기능 구현 완료

--- frontend/src/pages/auth/Terms/Terms.tsx
+++ frontend/src/pages/auth/Terms/Terms.tsx
@@ -7,6 +7,7 @@ function Terms({ navigation }: TermsNavigationProps) {
   return (
     <View>
       <Text>이용약관 페이지입니다.</Text>
+      <Button title="이용약관 확인하기" onPress={() => navigation.push('ReadTerms')} />
       <Button title="이용약관 동의하기" onPress={() => navigation.push('PersonalAuth')} />
     </View>
   );
