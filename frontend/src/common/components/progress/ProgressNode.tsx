86c2011b18dda370c99bc7b197311b44e6b0dd60
ikthegreat <ikthegreat@icloud.com>
2023-09-06 09:37:49 +0900

    refactor: 스타일시트 적용 및 폴더명 변경

--- frontend/src/common/components/progress/ProgressNode.tsx
+++ frontend/src/common/components/progress/ProgressNode.tsx
@@ -1,25 +1,27 @@
 import colors from '@common/design/colors';
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import { ProgressNodeProps } from '@common/interfaces/components';
-import Node from './Childs/Node';
-import Branch from './Childs/Branch';
+import Node from './childs/Node';
+import Branch from './childs/Branch';
 
 function ProgressNode({ page, size, current }: ProgressNodeProps) {
   const nodes = [];
   nodes.push(<Node size={size} color={colors.main} num={1} />);
   for (let i = 0; i < page - 1; i++) {
     nodes.push(
-      <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+      <View style={styles.flexBox} key={i}>
         <Branch size={size} color={current >= i + 2 ? colors.main : colors.disabled} />
         <Node size={size} color={current >= i + 2 ? colors.main : colors.disabled} num={i + 2} />
       </View>,
     );
   }
-  return (
-    <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>{nodes}</View>
-  );
+  return <View style={styles.flexBox}>{nodes}</View>;
 }
 
 export default ProgressNode;
+
+const styles = StyleSheet.create({
+  flexBox: { display: 'flex', flexDirection: 'row', alignItems: 'center' },
+});
