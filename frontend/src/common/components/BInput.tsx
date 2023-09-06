be01f7a21660dd06a182de9e7501061a0aa9c26b
yongseong2 <ancjs369@naver.com>
2023-09-06 10:40:45 +0900

    Feat: 인풋 컴포넌트 구현

--- frontend/src/common/components/BInput.tsx
+++ frontend/src/common/components/BInput.tsx
@@ -1,32 +1,69 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { View, Text, TextInput, StyleSheet, Animated } from 'react-native';
 import colors from '@common/design/colors';
 import { BInputProps } from '@common/interfaces/components';
 
 function BInput({ label, ...rest }: BInputProps) {
+  const [isFocused, setIsFocused] = useState(false);
+  const animatedIsFocused = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(animatedIsFocused, {
+      toValue: isFocused ? 1 : 0,
+      duration: 150,
+      useNativeDriver: false,
+    }).start();
+  }, [isFocused]);
+
+  const labelStyle = {
+    fontSize: animatedIsFocused.interpolate({
+      inputRange: [0, 1],
+      outputRange: [15, 10],
+    }),
+    paddingLeft: animatedIsFocused.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 11],
+    }),
+  };
+
+  const styles = StyleSheet.create({
+    input: {
+      width: '100%',
+      minHeight: 50,
+      padding: 8,
+      fontSize: 15,
+      margin: 2,
+    },
+    label: {
+      position: 'absolute',
+      color: isFocused ? colors.main : colors.disabled,
+      fontWeight: 'bold',
+    },
+    container: {
+      margin: 8,
+    },
+    hr: {
+      borderBottomWidth: 1,
+      borderBottomColor: isFocused ? colors.main : colors.disabled,
+      width: '95%',
+      position: 'absolute',
+      bottom: 10,
+      left: 10,
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>{label}</Text>
-      <TextInput {...rest} style={styles.input} placeholder={label} />
+      {isFocused && <Animated.Text style={[styles.label, labelStyle]}>{label}</Animated.Text>}
+      <TextInput
+        {...rest}
+        style={styles.input}
+        placeholder={isFocused ? '' : label}
+        onFocus={() => setIsFocused(true)}
+      />
+      <View style={styles.hr}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  input: {
-    borderBottomWidth: 1,
-    borderBottomColor: colors.disabled,
-    minHeight: 40,
-    padding: 8,
-    fontSize: 15,
-  },
-  label: {
-    fontSize: 10,
-    paddingLeft: 8,
-  },
-  container: {
-    margin: 5,
-  },
-});
 
 export default BInput;
