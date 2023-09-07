bff55d081fbab1ff6b3b7f36c2c6e9c2b2c8c9c9
ikthegreat <ikthegreat@icloud.com>
2023-09-07 16:23:29 +0900

    Feat: 쉐이크 이벤트 감지하여 Console.log 실행 (백그라운드 가능) 구현

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import RootStack from './src/navigator/RootStack';
+import React, { useEffect } from 'react';
+import { NativeEventEmitter, NativeModules } from 'react-native';
 import { Provider } from 'react-redux';
+import RootStack from './src/navigator/RootStack';
 import store from './src/store/store';
 
 function App(): JSX.Element {
+  const { EventListener } = NativeModules;
+
+  useEffect(() => {
+    EventListener.startListeningInBackground();
+  }, []);
+
+  const eventListener = new NativeEventEmitter(EventListener);
+
+  eventListener.addListener('onTrigger', (trigger) => {
+    console.log(trigger);
+  });
+
   return (
     <Provider store={store}>
       <RootStack />
