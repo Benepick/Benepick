14c838552dc4993cdaad3145d10fa434b6474340
psg980331@naver.com <psg980331@naver.com>
2023-09-12 13:15:25 +0900

    Feat: MyDataPayment 속성 수정

--- backend/src/main/java/com/ssafy/benepick/global/util/InitDb.java
+++ backend/src/main/java/com/ssafy/benepick/global/util/InitDb.java
@@ -120,73 +120,73 @@ public class InitDb {
 			MyDataPayment myDataPayment1 = MyDataPayment.builder().myDataPaymentId(1L)
 				.myDataPaymentCategory("카페").myDataPaymentDate(LocalDateTime.now())
 				.myDataPaymentAmount(10000).myDataPaymentCategory("카테1").myDataPaymentCategory2("카테2")
-				.myDataPaymentMerchantName("바나프레소").myDataCard(myDataCard1).myDataPaymentBenefit(100)
+				.myDataPaymentMerchantName("바나프레소").myDataCard(myDataCard1).myDataPaymentReceivedBenefitAmount(100)
 				.build();
 
 			MyDataPayment myDataPayment2 = MyDataPayment.builder().myDataPaymentId(2L)
 				.myDataPaymentCategory("식당").myDataPaymentDate(LocalDateTime.now())
 				.myDataPaymentAmount(20000).myDataPaymentCategory("카테1").myDataPaymentCategory2("카테2")
-				.myDataPaymentMerchantName("맥도날드").myDataCard(myDataCard1).myDataPaymentBenefit(200)
+				.myDataPaymentMerchantName("맥도날드").myDataCard(myDataCard1).myDataPaymentReceivedBenefitAmount(200)
 				.build();
 
 			MyDataPayment myDataPayment3 = MyDataPayment.builder().myDataPaymentId(3L)
 				.myDataPaymentCategory("카페").myDataPaymentDate(LocalDateTime.now())
 				.myDataPaymentAmount(30000).myDataPaymentCategory("카테1").myDataPaymentCategory2("카테2")
-				.myDataPaymentMerchantName("스타벅스").myDataCard(myDataCard2).myDataPaymentBenefit(300)
+				.myDataPaymentMerchantName("스타벅스").myDataCard(myDataCard2).myDataPaymentReceivedBenefitAmount(300)
 				.build();
 
 			MyDataPayment myDataPayment4 = MyDataPayment.builder().myDataPaymentId(4L)
 				.myDataPaymentCategory("쇼핑").myDataPaymentDate(LocalDateTime.now())
 				.myDataPaymentAmount(40000).myDataPaymentCategory("카테1").myDataPaymentCategory2("카테2")
-				.myDataPaymentMerchantName("ABC마트").myDataCard(myDataCard2).myDataPaymentBenefit(400)
+				.myDataPaymentMerchantName("ABC마트").myDataCard(myDataCard2).myDataPaymentReceivedBenefitAmount(400)
 				.build();
 
 			MyDataPayment myDataPayment5 = MyDataPayment.builder().myDataPaymentId(5L)
 				.myDataPaymentCategory("카페").myDataPaymentDate(LocalDateTime.now())
 				.myDataPaymentAmount(50000).myDataPaymentCategory("카테1").myDataPaymentCategory2("카테2")
-				.myDataPaymentMerchantName("매머드").myDataCard(myDataCard3).myDataPaymentBenefit(500)
+				.myDataPaymentMerchantName("매머드").myDataCard(myDataCard3).myDataPaymentReceivedBenefitAmount(500)
 				.build();
 
 			MyDataPayment myDataPayment6 = MyDataPayment.builder().myDataPaymentId(6L)
 				.myDataPaymentCategory("식당").myDataPaymentDate(LocalDateTime.now())
 				.myDataPaymentAmount(60000).myDataPaymentCategory("카테1").myDataPaymentCategory2("카테2")
-				.myDataPaymentMerchantName("탕후루").myDataCard(myDataCard3).myDataPaymentBenefit(600)
+				.myDataPaymentMerchantName("탕후루").myDataCard(myDataCard3).myDataPaymentReceivedBenefitAmount(600)
 				.build();
 
 			MyDataPayment myDataPayment7 = MyDataPayment.builder().myDataPaymentId(7L)
 				.myDataPaymentCategory("의료").myDataPaymentDate(LocalDateTime.now())
 				.myDataPaymentAmount(70000).myDataPaymentCategory("카테1").myDataPaymentCategory2("카테2")
-				.myDataPaymentMerchantName("역삼안과").myDataCard(myDataCard4).myDataPaymentBenefit(700)
+				.myDataPaymentMerchantName("역삼안과").myDataCard(myDataCard4).myDataPaymentReceivedBenefitAmount(700)
 				.build();
 
 			MyDataPayment myDataPayment8 = MyDataPayment.builder().myDataPaymentId(8L)
 				.myDataPaymentCategory("의료").myDataPaymentDate(LocalDateTime.now())
 				.myDataPaymentAmount(80000).myDataPaymentCategory("카테1").myDataPaymentCategory2("카테2")
-				.myDataPaymentMerchantName("설입성형외과").myDataCard(myDataCard6).myDataPaymentBenefit(800)
+				.myDataPaymentMerchantName("설입성형외과").myDataCard(myDataCard6).myDataPaymentReceivedBenefitAmount(800)
 				.build();
 
 			MyDataPayment myDataPayment10 = MyDataPayment.builder().myDataPaymentId(9L)
 				.myDataPaymentCategory("카페").myDataPaymentDate(LocalDateTime.now())
 				.myDataPaymentAmount(10000).myDataPaymentCategory("카테1").myDataPaymentCategory2("카테2")
-				.myDataPaymentMerchantName("바나프레소").myDataCard(myDataCard1).myDataPaymentBenefit(200)
+				.myDataPaymentMerchantName("바나프레소").myDataCard(myDataCard1).myDataPaymentReceivedBenefitAmount(200)
 				.build();
 
 			MyDataPayment myDataPayment11 = MyDataPayment.builder().myDataPaymentId(10L)
 				.myDataPaymentCategory("식당").myDataPaymentDate(LocalDateTime.now())
 				.myDataPaymentAmount(20000).myDataPaymentCategory("카테1").myDataPaymentCategory2("카테2")
-				.myDataPaymentMerchantName("맥도날드").myDataCard(myDataCard1).myDataPaymentBenefit(500)
+				.myDataPaymentMerchantName("맥도날드").myDataCard(myDataCard1).myDataPaymentReceivedBenefitAmount(500)
 				.build();
 
 			MyDataPayment myDataPayment12 = MyDataPayment.builder().myDataPaymentId(11L)
 				.myDataPaymentCategory("카페").myDataPaymentDate(LocalDateTime.now())
 				.myDataPaymentAmount(30000).myDataPaymentCategory("카테1").myDataPaymentCategory2("카테2")
-				.myDataPaymentMerchantName("스타벅스").myDataCard(myDataCard5).myDataPaymentBenefit(800)
+				.myDataPaymentMerchantName("스타벅스").myDataCard(myDataCard5).myDataPaymentReceivedBenefitAmount(800)
 				.build();
 
 			MyDataPayment myDataPayment13 = MyDataPayment.builder().myDataPaymentId(12L)
 				.myDataPaymentCategory("쇼핑").myDataPaymentDate(LocalDateTime.now())
 				.myDataPaymentAmount(40000).myDataPaymentCategory("카테1").myDataPaymentCategory2("카테2")
-				.myDataPaymentMerchantName("ABC마트").myDataCard(myDataCard5).myDataPaymentBenefit(1000)
+				.myDataPaymentMerchantName("ABC마트").myDataCard(myDataCard5).myDataPaymentReceivedBenefitAmount(1000)
 				.build();
 
 			bankBenepickEntityManager.persist(myDataPayment1);
