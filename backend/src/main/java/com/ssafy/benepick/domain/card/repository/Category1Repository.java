b79561c5cdbdadaf3cd7943c8beef6c4f9417a36
psg980331@naver.com <psg980331@naver.com>
2023-09-18 16:02:53 +0900

    Feat: 카드사 연동시 카드,혜택,카테고리 데이터 갱신

---
+++ backend/src/main/java/com/ssafy/benepick/domain/card/repository/Category1Repository.java
@@ -0,0 +1,9 @@
+package com.ssafy.benepick.domain.card.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.benepick.domain.card.entity.Card;
+import com.ssafy.benepick.domain.card.entity.Category1;
+
+public interface Category1Repository extends JpaRepository<Category1, Long> {
+}
--- backend/src/main/java/com/ssafy/benepick/domain/user/service/UserCardServiceImpl.java
+++ backend/src/main/java/com/ssafy/benepick/domain/user/service/UserCardServiceImpl.java
@@ -2,17 +2,24 @@ package com.ssafy.benepick.domain.user.service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.benepick.domain.card.entity.Category1;
+import com.ssafy.benepick.domain.card.service.CardService;
 import com.ssafy.benepick.domain.mydata.entity.MyDataCard;
 import com.ssafy.benepick.domain.mydata.entity.MyDataPayment;
 import com.ssafy.benepick.domain.user.entity.User;
 import com.ssafy.benepick.domain.user.entity.UserCard;
+import com.ssafy.benepick.domain.user.entity.UserCardBenefit;
+import com.ssafy.benepick.domain.user.entity.UserCardCategory1;
+import com.ssafy.benepick.domain.user.entity.UserCardCategory2;
 import com.ssafy.benepick.domain.user.entity.UserPayment;
+import com.ssafy.benepick.domain.user.repository.UserCardCategory1Repository;
 import com.ssafy.benepick.domain.user.repository.UserCardRepository;
 import com.ssafy.benepick.domain.user.repository.UserPaymentRepository;
 import com.ssafy.benepick.domain.user.repository.UserRepository;
@@ -28,6 +35,8 @@ public class UserCardServiceImpl implements  UserCardService{
 	private final UserCardRepository userCardRepository;
 	private final UserRepository userRepository;
 	private final UserPaymentRepository userPaymentRepository;
+	private final UserCardCategory1Repository userCardCategory1Repository;
+	private final CardService cardService;
 
 	@Override
 	@Transactional(transactionManager = "benepickTransactionManager")
@@ -38,12 +47,59 @@ public class UserCardServiceImpl implements  UserCardService{
 		myDataCardList.stream().forEach(myDataCard -> {
 			UserCard userCard = myDataCardToUserCard(myDataCard, user);
 			userCardRepository.save(userCard);
+
+			// 결제 내역 연동
 			List<UserPayment> userCardPaymentList = myDataCard.getMyDataPaymentList().stream()
 				.map(myDataPayment -> myDataPaymentToUserPayment(myDataPayment,userCard))
 				.collect(Collectors.toList());
+
+			List<UserCardCategory1> userCardCategory1List = new ArrayList<>();
+
+			// 카테고리1 연동
+			myDataCard.getCard().getCategory1List().stream().forEach(category1 -> {
+				UserCardCategory1 userCardCategory1 = category1.toUserCardCategory1(userCard);
+
+				category1.getCardBenefitList().stream().forEach(cardBenefit -> {
+					userCardCategory1.addUserCardBenefit(cardBenefit.toUserCardBenefit(userCardCategory1));
+				});
+
+				// 카테고리1 에대하여 카테고리2 연동
+				category1.getCategory2List().stream().forEach(category2 -> {
+					UserCardCategory2 userCardCategory2 = category2.toUserCardCategory2();
+
+					//카테고리2 에대하여 카테고리3 연동
+					category2.getCategory3List().stream().forEach(category3 -> {
+						userCardCategory2.addUserCardCategory3(category3.toUserCardCategory3());
+					});
+
+					userCardCategory1.addUserCardCategory2(userCardCategory2);
+				});
+				userCardCategory1List.add(userCardCategory1);
+			});
+
+			userCardCategory1Repository.saveAll(userCardCategory1List);
 			userPaymentRepository.saveAll(userCardPaymentList);
 		});
 	}
+
+	// private List<UserCardCategory1> toUserCardCategory1(List<Category1> category1List , UserCard userCard){
+	// 	List<UserCardCategory1> userCardCategory1List = new ArrayList<>();
+	//
+	// 	category1List.stream().forEach(category1 -> {
+	// 		category1.to
+	//
+	// 		// 혜택 정보들
+	// 		List<UserCardBenefit> userCardBenefitList = category1.getCardBenefitList()
+	// 			.stream()
+	// 			.map(cardBenefit -> cardBenefit.toUserCardBenefit(category1))
+	// 			.toList();
+	// 	});
+	//
+	//
+	//
+	// 	return userCardCategory1List;
+	// }
+
 
 	@Override
 	public UserCard myDataCardToUserCard(MyDataCard myDataCard,User user) {
