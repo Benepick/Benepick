f3da73c501060b85fd1a2cef426981e19263e503
ikthegreat <ikthegreat@icloud.com>
2023-09-18 14:29:34 +0900

    Feat: Tester 로그인 추가

--- frontend/src/pages/auth/Start/Start.tsx
+++ frontend/src/pages/auth/Start/Start.tsx
@@ -5,8 +5,11 @@ import { StartNavigationProps } from 'interfaces/navigation';
 import WhitePage from '@common/components/WhitePage';
 import SubmitButton from '@common/components/SubmitButton';
 import { Spacing } from '@common/components/Spacing';
+import { useAppDispatch } from '@store/hooks';
+import { setToken } from '@store/slices/userSlice';
 
 function Start({ navigation }: StartNavigationProps) {
+  const dispatch = useAppDispatch();
   return (
     <WhitePage style={styles.layout}>
       <Image style={styles.logo} source={require('@common/assets/images/logo/startLogo.png')} />
@@ -26,6 +29,17 @@ function Start({ navigation }: StartNavigationProps) {
       <Spacing />
 
       <SubmitButton title="테스트 페이지로 이동하기" onPress={() => navigation.push('Test')} />
+
+      <SubmitButton
+        title="최정수님으로 로그인하기"
+        onPress={() =>
+          dispatch(
+            setToken(
+              'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJpYXQiOjE2OTQ2NTI3MDAsImV4cCI6MTc1NTEzMjcwMCwidXNlcklkIjoiMDAyM2QwYjc2NDAxYzQ4NGMxMjNlOWRjYzYwMzQ5M2Y2ODdmZTRkZjhlOTY1MjFlOGU1MWFhMmVmZDNmZGEwZSJ9.FOVWaHgmS8QjEJYEHzn0rSTZNRsx3MX57VSql2PHWRs',
+            ),
+          )
+        }
+      />
     </WhitePage>
   );
 }
