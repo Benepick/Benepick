86bd4aa5acf0bf97d43a77a1c48df8cdee8816a1
hyunchul1998 <hyunchul1998@naver.com>
2023-09-19 10:51:04 +0900

    Fix: 내 카드 목록 조회 API 분리된 뱅킹 서버에 맞게 수정

--- backend/src/main/java/com/ssafy/benepick/domain/user/repository/UserCardCategory1Repository.java
+++ backend/src/main/java/com/ssafy/benepick/domain/user/repository/UserCardCategory1Repository.java
@@ -1,8 +1,12 @@
 package com.ssafy.benepick.domain.user.repository;
 
+import com.ssafy.benepick.domain.user.entity.UserCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.benepick.domain.user.entity.UserCardCategory1;
 
+import java.util.List;
+
 public interface UserCardCategory1Repository extends JpaRepository<UserCardCategory1,Long > {
+    List<UserCardCategory1> findByUserCard(UserCard usercard);
 }
