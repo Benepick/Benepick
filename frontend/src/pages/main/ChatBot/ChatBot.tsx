c2b297e49550d0bf6f895787d45c66f85e5e902b
yongseong2 <ancjs369@naver.com>
2023-09-20 10:19:17 +0900

    Feat: 챗봇페이지 구현

--- frontend/src/pages/main/ChatBot/ChatBot.tsx
+++ frontend/src/pages/main/ChatBot/ChatBot.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Button, Text, StyleSheet } from 'react-native';
 
 import { ChatBotNavigationProps } from 'interfaces/navigation';
 import Page from '@common/components/Page';
+import colors from '@common/design/colors';
 
 function ChatBot({ navigation }: ChatBotNavigationProps) {
   return (
-    <Page>
-      <Text>챗봇</Text>
-    </Page>
+    <View>
+      <View style={styles.chatRoom}>{/* <Text>챗봇</Text> */}</View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  chatRoom: {
+    width: '100%',
+    backgroundColor: colors.white,
+    height: '100%',
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    flexShrink: 1,
+  },
+});
 
 export default ChatBot;
