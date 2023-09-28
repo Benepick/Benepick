de0508f86fc824bd339d495e4faa4321588ea8c9
SSuSG <psg980331@naver.com>
2023-09-28 12:10:36 +0900

    Chore: 레디스 캐싱 설정 추가

---
+++ backend/src/main/java/com/ssafy/benepick/global/config/CacheConfig.java
@@ -0,0 +1,11 @@
+package com.ssafy.benepick.global.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+
+}
